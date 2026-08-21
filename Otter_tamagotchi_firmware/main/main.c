8f43359ea88494883e5dc77eecf62b635923507d
igorkll <frerther@gmail.com>
2026-08-21 20:55:46 +0300

    Update main.c

--- Otter_tamagotchi_firmware/main/main.c
+++ Otter_tamagotchi_firmware/main/main.c
@@ -27,16 +27,19 @@ static const char* TAG = "otter_tamagotchi";
 static tsgl_display display;
 static tsgl_framebuffer framebuffer;
 
+static tsgl_pos width;
+static tsgl_pos height;
+static tsgl_colormode colormode;
+
 static tsgl_rawcolor white;
-static tsgl_rawcolor red;
 static tsgl_rawcolor black;
 
 void drawCenteredImage(const char* path) {
-    tsgl_sprite* sprite = tsgl_bmp_load(path, settings.driver->colormode, BUFFER, tsgl_color_raw(tsgl_color_pack(0, 125, 0), display.colormode));
+    tsgl_sprite* sprite = tsgl_bmp_load(path, colormode, BUFFER, tsgl_color_raw(tsgl_color_pack(0, 125, 0), colormode));
 
     tsgl_framebuffer_push(&framebuffer,
-        (display.width / 2) - (sprite->sprite->width / 2),
-        (display.height / 2) - (sprite->sprite->height / 2),
+        (width / 2) - (sprite->sprite->width / 2),
+        (height / 2) - (sprite->sprite->height / 2),
         sprite
     );
 
@@ -44,10 +47,9 @@ void drawCenteredImage(const char* path) {
 }
 
 void app_main() {
-    tsgl_colormode colormode = settings.driver->colormode;
+    colormode = settings.driver->colormode;
 
     white = tsgl_color_raw(TSGL_WHITE, colormode);
-    red = tsgl_color_raw(TSGL_RED, colormode);
     black = tsgl_color_raw(TSGL_BLACK, colormode);
 
     settings.backlight_init = true;
@@ -58,19 +60,22 @@ void app_main() {
 
     ESP_ERROR_CHECK(tsgl_framebuffer_init(&framebuffer, colormode, settings.width, settings.height, BUFFER));
     tsgl_framebuffer_rotate(&framebuffer, FRAMEBUFFER_ROTATE);
-    size_t framebuffer_size = settings.width * settings.height * tsgl_colormodeSizes[colormode];
+    width = framebuffer.width;
+    height = framebuffer.height;
 
     tsgl_framebuffer_clear(&framebuffer, white);
     drawCenteredImage("/storage/test.bmp");
-    tsgl_framebuffer_fill(&framebuffer, 4, 4, 16, 16, tsgl_color_raw(TSGL_RED, display.colormode));
+    tsgl_framebuffer_fill(&framebuffer, 4, 4, 16, 16, black);
 
     settings.init_state = tsgl_display_init_framebuffer;
     settings.init_framebuffer_ptr = framebuffer.buffer;
-    settings.init_framebuffer_size = framebuffer_size;
+    settings.init_framebuffer_size = framebuffer.buffersize;
 
-    ESP_ERROR_CHECK(tsgl_spi_initManual(framebuffer_size, SPI, SPI_MOSI, SPI_MISO, SPI_CLK));
+    ESP_ERROR_CHECK(tsgl_spi_initManual(framebuffer.buffersize, SPI, SPI_MOSI, SPI_MISO, SPI_CLK));
     ESP_ERROR_CHECK(tsgl_display_spi(&display, settings, SPI, FREQ, DC, CS, RST));
 
+
+    tsgl_delay(5000);
     while (true) {
         tsgl_display_send(&display, &framebuffer);
         tsgl_delay(5000);
