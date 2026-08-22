90dba9c183361b5ad901963669b1c7c8619c90f7
igorkll <frerther@gmail.com>
2026-08-22 19:47:29 +0300

    qwe

--- Otter_tamagotchi_firmware/main/game.c
+++ Otter_tamagotchi_firmware/main/game.c
@@ -1,7 +1,28 @@
 #include "game.h"
 
+static const char* game_state_path = "/storage/game_state";
+
+static Game_state default_state = {
+    .room = 0
+};
+
+Game_state current_state;
+
+static void game_save() {
+    tsgl_filesystem_writeFile(game_state_path, &current_state, sizeof(Game_state))
+}
+
+static void game_load() {
+    if (tsgl_filesystem_exists(game_state_path)) {
+        tsgl_filesystem_readFile(game_state_path, &current_state, sizeof(Game_state));
+    } else {
+        memcpy(&current_state, &default_state, sizeof(Game_state));
+    }
+}
+
 void game_start() {
     ESP_LOGI(TAG, "game started!");
+    game_load();
 
     while (true) {
         tsgl_framebuffer_clear(&framebuffer, white);
--- Otter_tamagotchi_firmware/main/game.h
+++ Otter_tamagotchi_firmware/main/game.h
@@ -1,4 +1,16 @@
 #pragma once
 #include "main.h"
 
+typedef enum {
+    game_room_bedroom = 0,
+    game_room_kitchen
+} game_room;
+#define game_room uint8_t
+
+typedef struct {
+    game_room room;
+} Game_state;
+
+extern Game_state current_state;
+
 void game_start();
