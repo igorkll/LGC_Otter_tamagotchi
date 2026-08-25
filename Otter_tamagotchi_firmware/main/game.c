753efc32be62371fb958833e0b173a3b0291536c
igorkll <frerther@gmail.com>
2026-08-26 01:02:25 +0300

    Update game.c

--- Otter_tamagotchi_firmware/main/game.c
+++ Otter_tamagotchi_firmware/main/game.c
@@ -52,7 +52,7 @@ static tsgl_sound* play_sound(const char* path, int test) {
     static uint8_t current_sound_index = 0;
 
     tsgl_sound* current_sound = &sounds[current_sound_index];
-    if (tsgl_sound_load_pcmEx(current_sound, SOUND_BUFFER_SIZE, 0, path, test, 1, 1, tsgl_sound_pcm_unsigned, true) != ESP_OK) {
+    if (tsgl_sound_load_pcmEx(current_sound, SOUND_BUFFER_SIZE, 0, path, 8000, 1, 1, tsgl_sound_pcm_unsigned, true) != ESP_OK) {
         return NULL;
     }
     
