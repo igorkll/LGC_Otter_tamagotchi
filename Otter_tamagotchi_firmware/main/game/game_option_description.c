75462f2f27d8d19424c1333657c140ccca7813cc
igorkll <frerther@gmail.com>
2026-09-04 17:24:13 +0300

    Update game_option_description.c

--- Otter_tamagotchi_firmware/main/game/game_option_description.c
+++ Otter_tamagotchi_firmware/main/game/game_option_description.c
@@ -1,15 +1,15 @@
 #include "game_option_description.h"
 
-const char* car_str = "\xCC\xE0\xF8\xE8\xED\xE0"; //Машина
-const char* yard_str = "\xC4\xE2\xEE\xF0"; //Двор
-const char* shop_str = "\xCC\xE0\xE3\xE0\xE7\xE8\xED"; //Магазин
+#define CAR_STR   "\xCC\xE0\xF8\xE8\xED\xE0"
+#define YARD_STR  "\xC4\xE2\xEE\xF0"
+#define SHOP_STR  "\xCC\xE0\xE3\xE0\xE7\xE8\xED"
 
 const char* game_rooms_option_descriptions_main_rooms[GAME_UPMENU_LINE_COUNT] = {
     "\xD1\xEF\xE0\xEB\xFC\xED\xFF", //Спальня
     "\xCA\xF3\xF5\xED\xFF", //Кухня
     "\xCA\xEE\xEC\xED\xE0\xF2\xE0", //Комната
     "\xD2\xF3\xE0\xEB\xE5\xF2", //Туалет
-    yard_str //Двор
+    YARD_STR //Двор
 };
 
 Room_option_descriptions game_rooms_option_descriptions[] = {
@@ -76,7 +76,7 @@ Room_option_descriptions game_rooms_option_descriptions[] = {
             NULL,
             NULL,
             NULL,
-            car_str,
+            CAR_STR,
             NULL,
             NULL,
             NULL,
@@ -85,8 +85,8 @@ Room_option_descriptions game_rooms_option_descriptions[] = {
     },
     { //Машина
         .arr = {
-            yard_str,
-            shop_str,
+            YARD_STR,
+            SHOP_STR,
             NULL,
             NULL,
             NULL,
@@ -99,7 +99,7 @@ Room_option_descriptions game_rooms_option_descriptions[] = {
     },
     { //Магазин
         .arr = {
-            car_str,
+            CAR_STR,
             NULL,
             NULL,
             NULL,
