faf0871378259d44709dfd79666f567020509c75
igorkll <frerther@gmail.com>
2026-09-05 19:54:35 +0300

    qwe

--- Otter_tamagotchi_firmware/main/game/game_actions.c
+++ Otter_tamagotchi_firmware/main/game/game_actions.c
@@ -2,7 +2,7 @@
 #include "../pushsound.h"
 
 void game_actions_sleep(int sleepTime) {
-    pushsound_play("/firmware/sounds/sadness.pcm", 16000, EFFECTS_SOUND_VOLUME);
+    pushsound_play("/firmware/sounds/sadness.pcm", 16000, EFFECTS_SOUND_VOLUME * 3);
     current_state.sleepTimer = sleepTime;
     game_sleepIn();
 }
