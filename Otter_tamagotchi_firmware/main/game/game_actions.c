68950126f429bdb146a7eca43b288f6bd065682e
igorkll <frerther@gmail.com>
2026-09-08 01:20:05 +0300

    qwe

--- Otter_tamagotchi_firmware/main/game/game_actions.c
+++ Otter_tamagotchi_firmware/main/game/game_actions.c
@@ -8,3 +8,13 @@ void game_actions_sleep(int sleepTime) {
     current_state.sleepStartTimer = sleepTime;
     game_sleepIn();
 }
+
+void game_actions_eat() {
+    pushsound_play("/firmware/sounds/drinking.pcm", 16000, EFFECTS_SOUND_VOLUME * 5);
+    
+}
+
+void game_actions_drink() {
+    pushsound_play("/firmware/sounds/drinking.pcm", 16000, EFFECTS_SOUND_VOLUME * 5);
+    
+}
