1e7bf9bd806d01499b95727f2ec50ad2ac08a697
igorkll <frerther@gmail.com>
2026-09-07 23:00:55 +0300

    QWE

--- Otter_tamagotchi_firmware/main/game/cparts/rooms.h
+++ Otter_tamagotchi_firmware/main/game/cparts/rooms.h
@@ -4,12 +4,19 @@ const Room rooms[] = {
         .music = NULL,
         .person_x = (WIDTH / 4) * 3,
         .person_y = (HEIGHT / 4) * 3,
+
+        .states_delta_fatigue = -0.02,
+        .states_delta_thirst = 0.01,
+        .states_delta_sadness = 0.01
     },
     {
         .background = "kitchen",
         .music = NULL,
         .person_x = (WIDTH / 4) - 20,
         .person_y = (HEIGHT / 4) * 3,
+
+        .states_delta_thirst = 1,
+        .states_delta_sadness = 0.4
     },
     {
         .background = "gaming",
@@ -28,6 +35,10 @@ const Room rooms[] = {
         .music = NULL,
         .person_x = WIDTH / 4,
         .person_y = (HEIGHT / 4) * 2,
+
+        .states_delta_fatigue = -0.01,
+        .states_delta_thirst = 0.3,
+        .states_delta_sadness = -0.03
     },
     {
         .background = "car",
@@ -35,6 +46,11 @@ const Room rooms[] = {
         .musicVolume = 1,
         .person_x = (WIDTH / 4) * 3,
         .person_y = (HEIGHT / 4) * 3,
+
+        .states_delta_fatigue = 0.1,
+        .states_delta_hunger = 0.1,
+        .states_delta_thirst = 0.2,
+        .states_delta_sadness = -0.02
     },
     {
         .background = "shop",
@@ -43,10 +59,10 @@ const Room rooms[] = {
         .person_x = (WIDTH / 4) * 3,
         .person_y = (HEIGHT / 4) * 3,
 
-        .states_delta_fatigue = 1,
+        .states_delta_fatigue = 0.1,
         .states_delta_hunger = 1,
         .states_delta_thirst = 0.4,
-        .states_delta_sadness = 0.3
+        .states_delta_sadness = 0.2
     },
     {
         .background = "club",
@@ -55,9 +71,9 @@ const Room rooms[] = {
         .person_x = (WIDTH / 4) - 20,
         .person_y = (HEIGHT / 4) * 3,
 
-        .states_delta_fatigue = 1,
-        .states_delta_hunger = 0.2,
-        .states_delta_thirst = 0.3,
-        .states_delta_sadness = -0.2
+        .states_delta_fatigue = 0.05,
+        .states_delta_hunger = 0.1,
+        .states_delta_thirst = 0.2,
+        .states_delta_sadness = -0.1
     }
 };
