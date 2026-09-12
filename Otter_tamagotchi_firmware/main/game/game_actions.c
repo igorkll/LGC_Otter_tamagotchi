52c1c2801546274503c9da69dd907fad09776db3
igorkll <69525332+igorkll@users.noreply.github.com>
2026-09-12 16:05:08 +0300

    Update game_actions.c

--- Otter_tamagotchi_firmware/main/game/game_actions.c
+++ Otter_tamagotchi_firmware/main/game/game_actions.c
@@ -1,6 +1,7 @@
 #include "game_actions.h"
 #include "game_upmenu.h"
 #include "../pushsound.h"
+#include "game_states.h"
 
 void game_actions_sleep(int sleepTime) {
     pushsound_play("/firmware/sounds/sadness.pcm", 16000, EFFECTS_SOUND_VOLUME * SADNESS_SOUND_VOLUME);
@@ -11,10 +12,10 @@ void game_actions_sleep(int sleepTime) {
 
 void game_actions_eat() {
     pushsound_play("/firmware/sounds/eat.pcm", 16000, EFFECTS_SOUND_VOLUME);
-    
+    game_states_change(&current_state.states_hunger, -(100.0 / 2.0));
 }
 
 void game_actions_drink() {
     pushsound_play("/firmware/sounds/drinking.pcm", 16000, EFFECTS_SOUND_VOLUME);
-    
+    game_states_change(&current_state.states_thirst, -(100.0 / 3.0));
 }
