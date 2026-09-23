6bbb2e911380f21c97288262983a8936f428ac8d
igorkll <frerther@gmail.com>
2026-09-23 19:15:06 +0300

    writing nbs player

--- Otter_tamagotchi_firmware/TSGL/TSGL/TSGL.h
+++ Otter_tamagotchi_firmware/TSGL/TSGL/TSGL.h
@@ -24,6 +24,8 @@ typedef uint8_t tsgl_colormode_raw;
 #define TSGL_CALC_ARRSIZE(arr) (sizeof(arr) / sizeof(*(arr)))
 #define tsgl_rgb(_r, _g, _b) ((tsgl_color){.r = _r, .g = _g, .b = _b, .invalid = false})
 
+#define TSGL_MAX_PATH_LEN 256
+
 extern const tsgl_colormode_raw tsgl_colormodeSizes[];
 extern const bool tsgl_floatColormodeSizes[];
 
--- Otter_tamagotchi_firmware/TSGL/TSGL/TSGL_nbs.h
+++ Otter_tamagotchi_firmware/TSGL/TSGL/TSGL_nbs.h
@@ -5,32 +5,27 @@
 #define MAX_NOTES 32
 
 typedef struct {
-	tsgl_sound* samples;
+    tsgl_sound* samples;
     size_t count;
 } LoadedSamples;
 
 typedef struct {
-	uint8_t instrument;
-	float index;
-	float step;
-	bool play;
+    uint8_t instrument;
+    float index;
+    float step;
+    bool play;
 } Note;
 
 typedef struct {
-	FILE* file;
-	nbs_pcm_callback callback;
-	TaskHandle_t task;
-	gptimer_handle_t timer;
-	SemaphoreHandle_t mutex;
-	Note notes[MAX_NOTES];
-	float volume;
-	void* userArg;
+    FILE* file;
+    TaskHandle_t task;
+    Note notes[MAX_NOTES];
+
+    void* userData;
+    int userData_int;
+    float userData_float;
 } NBS;
 
-LoadedSamples* tsgl_nbs_loadSamples();
+LoadedSamples* tsgl_nbs_loadSamples(size_t count, const char* prefix, const char* suffix, size_t sample_rate, size_t bit_rate, size_t channels, tsgl_sound_pcm_format pcm_format);
+void tsgl_nbs_attachSamplesToOutputs(LoadedSamples* loadedSamples, tsgl_sound_output** outputs, size_t outputsCount, bool freeOutputs);
 void tsgl_nbs_freeSamples(LoadedSamples* loadedSamples);
-
-NBS* nbs_load(const char* path, nbs_pcm_callback callback, void* userArg);
-void nbs_play(NBS* nbs);
-void nbs_stop(NBS* nbs);
-void nbs_free(NBS* nbs);
